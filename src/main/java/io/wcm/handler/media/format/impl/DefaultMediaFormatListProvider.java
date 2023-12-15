eedc11f14793d2320474297d3aa66263bb673dc1
Stefan Seifert <stefanseifert@users.noreply.github.com>
2023-12-15 12:41:17 +0100

    Eliminate usage of org.apache.sling.commons.json (#26)

--- src/main/java/io/wcm/handler/media/format/impl/DefaultMediaFormatListProvider.java
+++ src/main/java/io/wcm/handler/media/format/impl/DefaultMediaFormatListProvider.java
@@ -20,7 +20,9 @@
 package io.wcm.handler.media.format.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 import javax.servlet.Servlet;
@@ -30,11 +32,13 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
-import org.apache.sling.commons.json.JSONArray;
-import org.apache.sling.commons.json.JSONException;
-import org.apache.sling.commons.json.JSONObject;
 import org.jetbrains.annotations.NotNull;
 import org.osgi.service.component.annotations.Component;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.wcm.handler.media.format.MediaFormat;
 import io.wcm.handler.media.format.MediaFormatHandler;
@@ -50,47 +54,39 @@ import io.wcm.wcm.commons.contenttype.FileExtension;
     "sling.servlet.resourceTypes=sling/servlet/default",
     "sling.servlet.methods=" + HttpConstants.METHOD_GET
 })
-@SuppressWarnings("deprecation")
 public final class DefaultMediaFormatListProvider extends SlingSafeMethodsServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
   protected void doGet(@NotNull SlingHttpServletRequest request, @NotNull SlingHttpServletResponse response) throws ServletException, IOException {
-    try {
+    // get list of media formats for current medialib path
+    Set<MediaFormat> mediaFormats = getMediaFormats(request);
 
-      // get list of media formats for current medialib path
-      Set<MediaFormat> mediaFormats = getMediaFormats(request);
-
-      response.setContentType(ContentType.JSON);
-
-      JSONArray mediaFormatList = new JSONArray();
-
-      if (mediaFormats != null) {
-        for (MediaFormat mediaFormat : mediaFormats) {
-          if (!mediaFormat.isInternal()) {
-            JSONObject mediaFormatItem = new JSONObject();
-            mediaFormatItem.put("name", mediaFormat.getName());
-            mediaFormatItem.put("text", mediaFormat.toString());
-            mediaFormatItem.put("width", mediaFormat.getWidth());
-            mediaFormatItem.put("height", mediaFormat.getHeight());
-            mediaFormatItem.put("widthMin", mediaFormat.getMinWidth());
-            mediaFormatItem.put("heightMin", mediaFormat.getMinHeight());
-            mediaFormatItem.put("widthHeightMin", mediaFormat.getMinWidthHeight());
-            mediaFormatItem.put("isImage", mediaFormat.isImage());
-            mediaFormatItem.put("ratio", mediaFormat.getRatio());
-            mediaFormatItem.put("ratioWidth", mediaFormat.getRatioWidthAsDouble());
-            mediaFormatItem.put("ratioHeight", mediaFormat.getRatioHeightAsDouble());
-            mediaFormatItem.put("ratioDisplayString", mediaFormat.getRatioDisplayString());
-            mediaFormatList.put(mediaFormatItem);
-          }
+    List<MediaFormatItem> mediaFormatList = new ArrayList<>();
+    if (mediaFormats != null) {
+      for (MediaFormat mediaFormat : mediaFormats) {
+        if (!mediaFormat.isInternal()) {
+          MediaFormatItem mediaFormatItem = new MediaFormatItem();
+          mediaFormatItem.name = mediaFormat.getName();
+          mediaFormatItem.text = mediaFormat.toString();
+          mediaFormatItem.width = mediaFormat.getWidth();
+          mediaFormatItem.height = mediaFormat.getHeight();
+          mediaFormatItem.widthMin = mediaFormat.getMinWidth();
+          mediaFormatItem.heightMin = mediaFormat.getMinHeight();
+          mediaFormatItem.widthHeightMin = mediaFormat.getMinWidthHeight();
+          mediaFormatItem.isImage = mediaFormat.isImage();
+          mediaFormatItem.ratio = mediaFormat.getRatio();
+          mediaFormatItem.ratioWidth = mediaFormat.getRatioWidthAsDouble();
+          mediaFormatItem.ratioHeight = mediaFormat.getRatioHeightAsDouble();
+          mediaFormatItem.ratioDisplayString = mediaFormat.getRatioDisplayString();
+          mediaFormatList.add(mediaFormatItem);
         }
       }
+    }
 
-      response.getWriter().write(mediaFormatList.toString());
-    }
-    catch (JSONException ex) {
-      throw new ServletException(ex);
-    }
+    // serialize to JSON using Jackson
+    response.setContentType(ContentType.JSON);
+    response.getWriter().write(new ObjectMapper().writeValueAsString(mediaFormatList));
   }
 
   protected Set<MediaFormat> getMediaFormats(SlingHttpServletRequest request) {
@@ -101,6 +97,72 @@ public final class DefaultMediaFormatListProvider extends SlingSafeMethodsServle
     else {
       return Collections.emptySet();
     }
+  }
+
+  @JsonInclude(Include.NON_NULL)
+  static class MediaFormatItem {
+    private String name;
+    private String text;
+    private long width;
+    private long height;
+    private long widthMin;
+    private long heightMin;
+    private long widthHeightMin;
+    private boolean isImage;
+    private double ratio;
+    private double ratioWidth;
+    private double ratioHeight;
+    private String ratioDisplayString;
+
+    public String getName() {
+      return this.name;
+    }
+
+    public String getText() {
+      return this.text;
+    }
+
+    public long getWidth() {
+      return this.width;
+    }
+
+    public long getHeight() {
+      return this.height;
+    }
+
+    public long getWidthMin() {
+      return this.widthMin;
+    }
+
+    public long getHeightMin() {
+      return this.heightMin;
+    }
+
+    public long getWidthHeightMin() {
+      return this.widthHeightMin;
+    }
+
+    @JsonProperty("isImage")
+    public boolean isImage() {
+      return this.isImage;
+    }
+
+    public double getRatio() {
+      return this.ratio;
+    }
+
+    public double getRatioWidth() {
+      return this.ratioWidth;
+    }
+
+    public double getRatioHeight() {
+      return this.ratioHeight;
+    }
+
+    public String getRatioDisplayString() {
+      return this.ratioDisplayString;
+    }
+
   }
 
 }
