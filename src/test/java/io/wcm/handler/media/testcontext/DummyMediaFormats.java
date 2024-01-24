547adc1a3231525916433aff09004ca291bc96a5
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-24 16:32:31 +0100

    remove flash sample media format

--- src/test/java/io/wcm/handler/media/testcontext/DummyMediaFormats.java
+++ src/test/java/io/wcm/handler/media/testcontext/DummyMediaFormats.java
@@ -34,7 +34,7 @@ public final class DummyMediaFormats {
       .label("Home Stage")
       .width(960)
       .height(485)
-      .extensions("gif", "jpg", "png", "swf")
+      .extensions("gif", "jpg", "png")
       .build();
 
   /* home_teaser */
@@ -213,16 +213,8 @@ public final class DummyMediaFormats {
       .label("Showroom Campaign Special")
       .width(960)
       .height(455)
-      .extensions("gif", "jpg", "png", "swf")
+      .extensions("gif", "jpg", "png")
       .ranking(10)
-      .build();
-
-  /* feature_flash */
-  public static final MediaFormat FEATURE_FLASH = create("feature_flash")
-      .label("Feature Flash Demo")
-      .width(570)
-      .height(270)
-      .extensions("gif", "jpg", "png", "swf")
       .build();
 
   /* wallpaper */
@@ -285,7 +277,7 @@ public final class DummyMediaFormats {
       .label("Editorial Stage Large")
       .width(960)
       .height(335)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .build();
 
   /* editorial_stage_medium */
@@ -293,7 +285,7 @@ public final class DummyMediaFormats {
       .label("Editorial Stage Medium")
       .width(705)
       .height(335)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .build();
 
   /* editorial_stage_small */
@@ -301,7 +293,7 @@ public final class DummyMediaFormats {
       .label("Editorial Stage Small")
       .width(960)
       .height(150)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .build();
 
   /* editorial_standard */
@@ -309,35 +301,35 @@ public final class DummyMediaFormats {
       .label("Editorial Standard")
       .width(1055)
       .height(500)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .renditionGroup("/apps/test/renditiongroup/editorial_standard")
       .build();
   public static final MediaFormat EDITORIAL_1COL = create("editorial_1col")
       .label("Editorial Standard 1 Column")
       .width(215)
       .height(102)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .renditionGroup("/apps/test/renditiongroup/editorial_standard")
       .build();
   public static final MediaFormat EDITORIAL_2COL = create("editorial_2col")
       .label("Editorial Standard 2 Columns")
       .width(450)
       .height(213)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .renditionGroup("/apps/test/renditiongroup/editorial_standard")
       .build();
   public static final MediaFormat EDITORIAL_3COL = create("editorial_3col")
       .label("Editorial Standard 3 Columns")
       .width(685)
       .height(325)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .renditionGroup("/apps/test/renditiongroup/editorial_standard")
       .build();
   public static final MediaFormat SPECIAL_4COL = create("special_4col")
       .label("Flash Special")
       .width(960)
       .height(455)
-      .extensions("gif","jpg","png","swf")
+      .extensions("gif", "jpg", "png")
       .renditionGroup("/apps/test/renditiongroup/editorial_standard")
       .ranking(30)
       .build();
